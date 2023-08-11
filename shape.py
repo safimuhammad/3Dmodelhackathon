841e4bee7433d46b5c6297a0605e6fbab1d1ac8f
MuzammilMeh <muzammil.mehmood@xloopdigital.com>
2023-08-12 00:23:05 +0500

    threding

--- shape.py
+++ shape.py
@@ -1,69 +1,100 @@
 from gradio_client import Client
 import shutil
 import os
+import threading
 import requests
 import trimesh
-import numpy as np
+from queue import Queue
 
-def get_model(prompt,filename,seed=1,guidance=16,steps=64):
+
+def get_model(prompt, filename, seed=1, guidance=16, steps=64):
     try:
-        flag=False
         client = Client("https://hysts-shap-e.hf.space/")
         result = client.predict(
-                        prompt,	# str in 'Prompt' Textbox component
-                        seed,	# int | float (numeric value between 0 and 2147483647) in 'Seed' Slider component
-                        guidance,	# int | float (numeric value between 1 and 20) in 'Guidance scale' Slider component
-                        steps,	# int | float (numeric value between 1 and 100) in 'Number of inference steps' Slider component
-                        api_name="/text-to-3d"
+            prompt,
+            seed,
+            guidance,
+            steps,
+            api_name="/text-to-3d",
         )
         if result:
-            project_directory = f"models"  # Change this to your actual project directory path
-            output_filename = f"{filename}.glb"  # Change this to the desired output filename
+            project_directory = "models"
+            output_filename = f"{filename}.glb"
 
             output_path = os.path.join(project_directory, output_filename)
             if os.path.exists(result):
                 shutil.move(result, output_path)
                 print(f"File moved to: {output_path}")
-                flag=True
+                return output_path
         else:
-            flag=False
             print("Prediction failed or no result received.")
-
     except requests.exceptions.RequestException as req_err:
         print(f"An error occurred during the request: {req_err}")
     except TimeoutError as timeout_err:
         print(f"Request timed out: {timeout_err}")
     except Exception as general_err:
         print(f"An error occurred: {general_err}")
-    
-    return flag
+    return None
 
-def combine_model(model1,model2,filename):
+
+def combine_model(model1, model2, filename):
     chair_glb = trimesh.load(model1)
-    chair_glb.export(f'{model1[:-4]}.obj')
-    chair = trimesh.load_mesh(f'{model1[:-4]}.obj')
+    chair_glb.export(f"{model1[:-4]}.obj")
+    chair = trimesh.load_mesh(f"{model1[:-4]}.obj")
 
-   
     seat_glb = trimesh.load(model2)
-    seat_glb.export(f'{model2[:-4]}.obj')
-    seat = trimesh.load_mesh(f'{model2[:-4]}.obj')
+    seat_glb.export(f"{model2[:-4]}.obj")
+    seat = trimesh.load_mesh(f"{model2[:-4]}.obj")
     combined = trimesh.util.concatenate(chair, seat)
-    
+
     # Save the combined mesh to a new .obj file
-    combined.export(f'models/{filename}.glb')
+    combined.export(f"models/{filename}.glb")
 
-if __name__ =="__main__":
-    prompt1="""Create a chair with four 30 cm legs and a 30 cm wide, 40 cm deep seat. 
-    The backrest should be 35 cm wide and 40 cm tall,chair standing. The chair can be any material and color."""
 
-    prompt2="""“Create a soft, rectangular pillow,
-     15 cm wide and 15 cm deep laying horizontally, to fit on a chair’s seat. 
-     The pillow can have blue color and any pattern."""
+def process_prompt(prompt, filename, seed=1, guidance=16, steps=64, result_queue=None):
+    result = get_model(prompt, filename, seed, guidance, steps)
+    if result:
+        model_path = f"models/{filename}.glb"
+        result_queue.put(model_path)
+    else:
+        result_queue.put(None)
 
-    # handle muilti prompts with multi threading 
 
-    get_model(prompt1,'test_chair',seed=4356)
-    get_model(prompt2,'test_seat',seed=341)
-    model1='models/test_chair.glb'
-    model2='models/test_seat.glb'
-    combine_model(model1,model2,'check')
+if __name__ == "__main__":
+    prompts = [
+        (
+            """Render a only rectangular tabletop for a standard dining table. The tabletop dimensions should be 120 cm in length and 80 cm in width, table color should be black.""",
+            "table_top",
+            1236,
+        ),
+        (
+            """Render only four identical table legs for the The tabletop dimensions should be 120 cm in length and 80 cm in width. Each leg should have a height of 75 cm.""",
+            "table_legs",
+            5678,
+        ),
+        # You can keep adding more prompts for different chair parts here
+    ]
+
+    threads = []
+    result_queue = Queue()
+
+    for prompt, filename, seed in prompts:
+        thread = threading.Thread(
+            target=process_prompt, args=(prompt, filename, seed, 16, 64, result_queue)
+        )
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    model_paths = [result_queue.get() for _ in prompts]
+
+    # Check if all models were successfully generated
+    if all(model_path is not None for model_path in model_paths):
+        # Combine the models
+        combine_model(model_paths[0], model_paths[1], "final_chair_model")
+        # You can add more combining logic here for additional chair parts
+        print("All chair components successfully combined into a final model.")
+    else:
+        print("One or more chair components could not be generated.")
