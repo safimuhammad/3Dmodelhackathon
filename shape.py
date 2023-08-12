a84b6c42a5e33ead6a7a2eb40266802e462c9bb4
safimuhammad <safiwahid38@gmail.com>
2023-08-13 04:32:55 +0500

    colors added

--- shape.py
+++ shape.py
@@ -48,7 +48,18 @@ def combine_model(model1=None, model2=None, filename=None,color1=None,color2=Non
     'cyan': [0, 255, 255, 255],
     'magenta': [255, 0, 255, 255],
     'white': [255, 255, 255, 255],
-    'black': [0, 0, 0, 255]}
+    'black': [0, 0, 0, 255],
+    'orange': [255, 165, 0],
+    'purple': [128, 0, 128],
+    'pink': [255, 192, 203],
+    'brown': [165, 42, 42],
+    'gray': [128, 128, 128],
+    'maroon': [128, 0, 0],
+    'olive': [128, 128, 0],
+    'navy': [0, 0, 128],
+    'teal': [0, 128, 128],
+    
+    }
 
    
 
